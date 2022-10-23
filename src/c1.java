cda1688cb032589d3539fb982e8edb3a66185a17
IT20128036 <87744181+IT20128036@users.noreply.github.com>
2022-10-23 20:52:07 +0530

    update c1

--- src/c1.java
+++ src/c1.java
@@ -1,20 +1,23 @@
 
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.io.IOException;
 import java.util.Properties;
 
-
 public class c1 {
 
 	public static final Properties p = new Properties();
+	public static final Logger log = Logger.getLogger(Exception.class.getName());
 
 	static {
 		try {
 			p.load(c2.class.getResourceAsStream("../config/config.properties"));
+		} catch (IOException e) {
+			log.log(Level.SEVERE, e.getMessage());
+
 		} catch (Exception e) {
-			
+			log.log(Level.SEVERE, e.getMessage());
+
 		}
 	}
 }
