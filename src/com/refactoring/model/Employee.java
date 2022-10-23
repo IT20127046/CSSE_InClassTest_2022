06fa5604172040c5706cd1272c82f97a34256d5e
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-10-23 23:47:28 +0530

    Update folder structure

--- src/com/refactoring/model/Employee.java
+++ src/com/refactoring/model/Employee.java
@@ -1,3 +1,4 @@
+package com.refactoring.model;
 public class Employee {
 
 	public String employeeId;
--- src/com/refactoring/util/CommonUtil.java
+++ src/com/refactoring/util/CommonUtil.java
@@ -1,17 +1,18 @@
+package com.refactoring.util;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.io.IOException;
 import java.util.Properties;
 
-public class c1 {
+public class CommonUtil {
 
 	public static final Properties p = new Properties();
 	public static final Logger log = Logger.getLogger(Exception.class.getName());
 
 	static {
 		try {
-			p.load(c2.class.getResourceAsStream("../config/config.properties"));
+			p.load(Query.class.getResourceAsStream("./config.properties"));
 		} catch (IOException e) {
 			log.log(Level.SEVERE, e.getMessage());
 
