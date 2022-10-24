f86b3d8b0230b0fe2e8ae80a334e242d81f3d495
IT20128036 <87744181+IT20128036@users.noreply.github.com>
2022-10-24 13:04:09 +0530

    update

--- src/com/refactoring/service/EmployeeService.java
+++ src/com/refactoring/service/EmployeeService.java
@@ -3,6 +3,7 @@ package com.refactoring.service;
 import com.refactoring.model.Employee;
 import com.refactoring.util.CommonConstants;
 import com.refactoring.util.CommonUtil;
+import com.refactoring.util.DBConnectionUtil;
 import com.refactoring.util.Query;
 import com.refactoring.util.Transformation;
 import java.io.IOException;
@@ -28,6 +29,7 @@ public class EmployeeService extends CommonUtil {
 	private PreparedStatement preparedStatement;
 	public static final Logger log = Logger.getLogger(EmployeeService.class.getName());
 
+
 	
 
 
@@ -37,13 +39,12 @@ public class EmployeeService extends CommonUtil {
 
 	public EmployeeService() {
 		try {
-			Class.forName(CommonConstants.CLASSNAME);
-			connection = DriverManager.getConnection(properties.getProperty(CommonConstants.URL),
-					properties.getProperty(CommonConstants.USERNAME), properties.getProperty(CommonConstants.PASSWORD));
+			connection = DBConnectionUtil.getDBConnection();
+			
 
 		} catch (SQLException e) {
 			log.log(Level.SEVERE, e.getMessage());
-		} catch (Exception e) {
+		} catch (ClassNotFoundException e) {
 			log.log(Level.SEVERE, e.getMessage());
 		}
 	}
